5267e959115464e1d7d50fe4cbd250989ba3fc44
Selina Swampillai <sswampil@gmail.com>
2025-05-30 08:41:34 -0400

    Initial commit with Playwright test and GitHub Actions workflow

---
+++ login.spec.js
@@ -0,0 +1,8 @@
+const { test, expect} = require('@playwright/test');
+
+test('Homepage Loads', async ({page}) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+
