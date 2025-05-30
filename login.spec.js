bd29c9d4ebbf4de346ea16e967df5b03977de543
Selina Swampillai <sswampil@gmail.com>
2025-05-30 08:49:23 -0400

    Trigger GitHub Actions workflow

--- login.spec.js
+++ login.spec.js
@@ -1,8 +1,8 @@
-const { test, expect} = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('Homepage Loads', async ({page}) => {
-  await page.goto('https://example.com');
+test("Homepage Loads", async ({ page }) => {
+  await page.goto("https://example.com");
   await expect(page).toHaveTitle(/Example Domain/);
 });
 
-
+//// Just a test change to trigger GitHub Actions
